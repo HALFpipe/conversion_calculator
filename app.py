d3c002c991919eb2700417171ed55c11e5b944ff
Jason A. Staph <jstaph@staph.us>
2023-07-04 14:11:22 -0400

    Add jinja2 update UI to kinda working

--- app.py
+++ app.py
@@ -1,51 +1,43 @@
 import conversion_calculator
+import io
 import shinyswatch
 from shiny import App, render, ui, reactive
-
-
-def ui_card(title, *args):
-    return (
-        ui.div(
-            {"class": "card mb-4"},
-            ui.div(title, class_="card-header"),
-            ui.div({"class": "card-body"}, *args),
-        ),
-    )
+from typing import List
 
 
 app_ui = ui.page_fluid(
     shinyswatch.theme.superhero(),
-    ui_card(
-        "Download the verbal learning template",
-        ui.download_button("verbal_learning_download_button", "Download CSV"),
-    ),
-    ui_panel_conditional(
-        "download_converted_csv", "input.csv_convert",
-        ui.download_button("csv_convert", "Download CSV"),
-    ),
-    ui_card(
-        "Convert a complete verbal learning template",
-        ui.input_file("csv_convert", "File input:"),
-    ),
-    title="Conversion Calculator",
+    ui.input_file("file1", "Choose a file to upload:", multiple=True),
+    ui.output_table("file_content"),
+    ui.download_button("download_conversion", "Download Conversion"),
 )
 
-
 def server(input, output, session):
-    MAX_FILE_SIZE = 1000000
+    MAX_SIZE = 50000
 
-    @session.download()
-    def verbal_learning_download_button():
-        return conversion_calculator.lib.get_csv_template_path()
-
-    @session.download()
-    def csv_convert(input_csv):
-        if not input_csv:
+    @output
+    @render.table
+    def file_content():
+        file_infos = input.file1()
+        if not file_infos:
             return
 
-        input_df = conversion_calculator.lib.parse_input_csv(input_csv)
-        converted_df = conversion_calculator.lib.convert_spreadsheet(input_df)
-        return conversion_calculator.lib.dataframe_to_csv(converted_df)
+        if len(file_infos) > 1:
+            return
+        
+        return conversion_calculator.lib.parse_input_csv(file_infos[0]['datapath'])
+    
+    @session.download()
+    def download_conversion():
+        file_infos = input.file1()
+        if not file_infos:
+            return
 
+        if len(file_infos) > 1:
+            return
+
+        input_df = conversion_calculator.lib.parse_input_csv(file_infos[0]['datapath'])
+        converted_df = conversion_calculator.lib.convert_spreadsheet(input_df)
+        return io.BytesIO(conversion_calculator.lib.dataframe_to_csv(converted_df).encode('utf-8'))
 
 app = App(app_ui, server)
