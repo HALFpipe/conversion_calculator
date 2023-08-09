f3392377c920851d3661ac19f857b664a5564351
Jason A. Staph <jstaph@staph.us>
2023-08-08 21:17:57 -0400

    add rule-engine, start refactoring crosswalks, add dr<->ldrf equivalence

--- src/conversion_calculator/crosswalks/__init__.py
+++ src/conversion_calculator/crosswalks/__init__.py
@@ -2,6 +2,12 @@ import numpy as np
 
 from conversion_calculator.models import CrossWalk, Instrument, InstrumentItem, Trial
 
+from .cvlt_ldrf import cvlt_ldrf
+
+__all__ = [
+    "cvlt_ldrf",
+]
+
 instrument_cvlt = Instrument(id="cvlt")
 instrument_hvlt = Instrument(id="hvlt")
 instrument_ravlt = Instrument(id="ravlt")
@@ -11,33 +17,6 @@ instrument_item_sdfr = InstrumentItem(id="sdfr")
 
 instrument_trial_t1 = Trial(id="t1")
 instrument_trial_t15 = Trial(id="t15")
-
-cvlt_ldrf = CrossWalk(
-    instrument=instrument_cvlt,
-    instrument_item=instrument_item_ldfr,
-    column_order={"cvlt": 0, "ravlt": 1, "hvlt": 2},
-    lookup_table=np.array(
-        [
-            [0, 0, 0],
-            [1, 2, 3],
-            [2, 2, 3],
-            [3, 3, 4],
-            [4, 4, 5],
-            [5, 5, 5],
-            [6, 6, 6],
-            [7, 7, 6],
-            [8, 8, 7],
-            [9, 8, 8],
-            [10, 9, 8],
-            [11, 10, 9],
-            [12, 11, 9],
-            [13, 12, 10],
-            [14, 12, 10],
-            [15, 13, 11],
-            [16, 14, 12],
-        ]
-    ),
-)
 
 cvlt_sdfr = CrossWalk(
     instrument=instrument_cvlt,
